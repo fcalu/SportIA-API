80705af0bb13acdfd69645f6548993c8693e861b
fcalu <superoddsplay@gmail.com>
2026-02-14 15:30:38 -0600

    mensaje

--- app/services/predictor.py
+++ app/services/predictor.py
@@ -222,6 +222,10 @@ async def ai_predict(req):
 
             home_stats = await get_team_stats(league, home_id)
             away_stats = await get_team_stats(league, away_id)
+            
+            # 🔥 AGREGA ESTO AQUÍ
+            debug("HOME_STATS", home_stats)
+            debug("AWAY_STATS", away_stats)
 
             xg = expected_goals_match(
                 home_stats, away_stats, league
